0cb2f60b59ddfddee37061f10cace7678ef55ad0
Maria Gargoles <mariagargoles@live.com>
2024-07-16 13:13:21 +0200

    ADD: json task

--- pages/task.php
+++ pages/task.php
@@ -4,5 +4,10 @@ $Task_Name = $_POST['Task_Name'] ?? '';
 $Task_Name = trim($Task_Name);
 
 if ($Task_Name) {
-    echo "Save To-do";
+    $filePath = __DIR__ . '/../data/tasks.json';
+
+    if (file_exists($filePath)) {
+        $json = file_get_contents($filePath);
+        echo $json;
+    }
 }
