52e74df843cf1589eb87155d0e7627bd64ff3eb0
Maria Gargoles <mariagargoles@live.com>
2024-07-16 13:45:29 +0200

    ADD: add to to-do list

--- pages/task.php
+++ pages/task.php
@@ -5,9 +5,13 @@ $Task_Name = trim($Task_Name);
 
 if ($Task_Name) {
     $filePath = __DIR__ . '/../data/tasks.json';
+    $json = file_get_contents($filePath);
+    $jsonArray = json_decode($json, true);
+    $jsonArray[] = [
+        'name' => $Task_Name,
+        'completed' => false
+    ];
 
-    if (file_exists($filePath)) {
-        $json = file_get_contents($filePath);
-        echo $json;
-    }
+    // var_dump($jsonArray);
+    file_put_contents($filePath, json_encode($jsonArray, JSON_PRETTY_PRINT));
 }
