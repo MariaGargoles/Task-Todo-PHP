dd9216038efe517cfb49a9b7ce6952b696c20465
Maria Gargoles <mariagargoles@live.com>
2024-07-16 15:52:09 +0200

    FIX: Restructuring the JSON for the test, functions had to be added and the code had to be restructured.

--- pages/delete.php
+++ pages/delete.php
@@ -2,17 +2,23 @@
 
 $filePath = __DIR__ . '/../data/tasks.json';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['TaskName'])) {
-    $TaskName = trim($_POST['TaskName']);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['TaskID'])) {
+    $TaskID = (int)$_POST['TaskID'];
 
     if (file_exists($filePath)) {
         $json = file_get_contents($filePath);
         $jsonArray = json_decode($json, true);
 
-        if (isset($jsonArray[$TaskName])) {
-            unset($jsonArray[$TaskName]);
-            file_put_contents($filePath, json_encode($jsonArray, JSON_PRETTY_PRINT));
+        foreach ($jsonArray as $index => $task) {
+            if ($task['id'] === $TaskID) {
+                unset($jsonArray[$index]);
+                $jsonArray = array_values($jsonArray);
+                break;
+            }
         }
+
+        file_put_contents($filePath, json_encode($jsonArray, JSON_PRETTY_PRINT));
     }
 }
 
