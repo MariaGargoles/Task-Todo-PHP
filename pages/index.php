a4dffbc659b3c46c63ba3d6be7d84291c98205d0
MariaGargoles <mariagargoles@live.com>
2024-07-17 13:40:55 +0200

    ADD: basic styles to TodoList

--- pages/index.php
+++ pages/index.php
@@ -13,30 +13,37 @@ if (file_exists($filePath)) {
 
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="../styles/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My PHP To-do List</title>
+
 </head>
 
 <body>
-    <section>
-        <form action="task.php" method="post">
-            <input type="text" name="Task_Title" placeholder="Task Title" required />
-            <input type="text" name="Task_Description" placeholder="Task Description" required />
-            <button type="submit">Add Task</button>
-        </form>
-    </section>
-    <section>
-        <?php foreach ($TaskItems as $task) : ?>
-            <div>
-                <input type="checkbox" <?php echo $task['completed'] ? 'checked' : '' ?> disabled />
-                <?php echo ($task['title']); ?>:
-                <?php echo ($task['description']); ?>
-                <form action="delete.php" method="post" style="display:inline;">
-                    <input type="hidden" name="TaskID" value="<?php echo ($task['id']); ?>" />
-                    <button type="submit">Delete</button>
-                </form>
-            </div>
-        <?php endforeach; ?>
+
+    <section class="SectionForm">
+        <h3 class="SectionForm__Title">TO - DO List</h3>
+        <article class="ArticleTaskForm">
+            <form class="ArticleTaskForm__Form" action="task.php" method="post">
+                <input class="ArticleTaskForm__Input" type="text" name="Task_Title" placeholder="Task Title" required />
+                <input class="ArticleTaskForm__Input" type="text" name="Task_Description" placeholder="Task Description" required />
+                <button class="ArticleTaskForm__Button" type="submit">Add Task</button>
+            </form>
+        </article>
+        <article class="ArticleTask__List">
+            <?php foreach ($TaskItems as $task) : ?>
+                <div>
+                    <input type="checkbox" <?php echo $task['completed'] ? 'checked' : '' ?> disabled />
+                    <?php echo ($task['title']); ?>:
+                    <?php echo ($task['description']); ?>
+                    <form action="delete.php" method="post" style="display:inline;">
+                        <input type="hidden" name="TaskID" value="<?php echo ($task['id']); ?>" />
+                        <button class="ArticleTask__Button" type="submit">Delete</button>
+                    </form>
+                </div>
+            <?php endforeach; ?>
+        </article>
     </section>
 </body>
 
